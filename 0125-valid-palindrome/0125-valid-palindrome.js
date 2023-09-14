e9ca9c0af04f9ff4736f68dc9b8bcff67b43a0ff
문형석 <110374331+RalphMoon@users.noreply.github.com>
2023-09-14 14:13:49 +0900

    Time: 82 ms (13.52%), Space: 51.9 MB (7.20%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.js
@@ -0,0 +1,22 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isPalindrome = function (s) {
+  const isAlphanumeric = function (char) {
+    const unicode = char.charCodeAt(0);
+    
+    if (unicode >= 48 && unicode <= 57 ||
+        unicode >= 65 && unicode <= 90 ||
+        unicode >= 97 && unicode <= 122) return true;
+    
+    return false;
+  };
+  const filteredLetters = s.split("").filter((char) => isAlphanumeric(char)).map((char) => char.toLowerCase());
+  const filteredString = filteredLetters.join("");
+  const reversedString = filteredLetters.slice().reverse().join("");
+  
+  console.log(filteredString, reversedString);
+  
+  return filteredString === reversedString;
+};
