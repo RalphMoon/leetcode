d81a274e5531d01c2a2cd42281a4b8d8ed891a68
문형석 <110374331+RalphMoon@users.noreply.github.com>
2023-09-19 11:53:07 +0900

    Time: 60 ms (38.30%), Space: 42.7 MB (38.21%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+const isValid = function (s) {
+  const stack = [];
+  
+  for (const char of s) {
+    if (char === "(" || char === "{" || char === "[") {
+      stack.push(char);
+      continue;
+    }
+    
+    if (char === ")" && stack[stack.length - 1] !== "(" ||
+        char === "}" && stack[stack.length - 1] !== "{" ||
+        char === "]" && stack[stack.length - 1] !== "[") {
+      return false;
+    }
+
+    stack.pop();
+  }
+  
+  return !stack.length;
+};
